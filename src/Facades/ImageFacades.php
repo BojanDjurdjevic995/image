b8cc4429ecc4c6f0c5aa69222b207a2feaed89c2
bojan.djurdjevic <djurdjevicbojan12@gmail.com>
2020-09-29 08:21:47 +0200

    Edit composer and store method

--- src/Facades/ImageFacades.php
+++ src/Facades/ImageFacades.php
@@ -2,6 +2,7 @@
 namespace Baki\Image\Facades;
 
 use Image;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Storage;
 
@@ -12,14 +13,14 @@ class ImageFacades extends Facade
         return 'StoreImage';
     }
 
-    protected static function store($input_file, $storage_disk, $slug, $request, $defaultPictures, $edit = false, $imageOld = '', $width = false, $height = false)
+    protected static function store($input_file, $storage_disk, $slug, Request $request, $defaultPictures, $edit = false, $imageOld = '', $width = false, $height = false)
     {
         if ($request->hasFile($input_file))
         {
-            $image = $request->file($input_file);
-            $name = $slug . '-' . time() . '.' . $image->getClientOriginalExtension();
-            $makeImage = ($width && $height) ? Image::make($image)->fit($width, $height) : Image::make($image);
-            $path = Storage::disk($storage_disk)->path($name);
+            $image      = $request->file($input_file);
+            $name       = $slug . '-' . time() . '.' . $image->getClientOriginalExtension();
+            $makeImage  = ($width && $height) ? Image::make($image)->fit($width, $height) : Image::make($image);
+            $path       = Storage::disk($storage_disk)->path($name);
             if(Storage::disk($storage_disk)->put($name, $makeImage->save($path)))
             {
                 if($edit && $imageOld != NULL && $imageOld != $defaultPictures && Storage::disk($storage_disk)->exists($imageOld))
